2189b76c4595a9dec40e35165c95815a31147a97
Maurice Clere <cleremaurice@gmail.com>
2017-04-11 22:32:19 +0200

    secondHandBikes site added

--- bike_net.R
+++ bike_net.R
@@ -2,6 +2,7 @@
 # Web scrapping bikes (with shimano XP) in Sweden and Denmark
 
 library(rvest)  # hmtl parsing package
+
 blocket_update <- function(blocket_url,bike_list){
       
       html_block <- read_html(blocket_url)
@@ -14,25 +15,25 @@ blocket_update <- function(blocket_url,bike_list){
       
       while (!(ID[i] %in% bike_list$id) & !(i > length(ID))){  # a checker
             
-            #title & link    
+            # title & link    
             CSS_selector <- paste("#",ID[i]," > div:nth-child(2) > h1:nth-child(2) > a:nth-child(1)",sep = "" )
             sub_block <- html_nodes(html_block, CSS_selector)
             title <- html_attr(x = sub_block,name = "title")
             link <- html_attr(x = sub_block,name = "href")
             
-            # Extract date
+            # date
             CSS_selector <- paste("#",ID[i]," > div:nth-child(2) > header:nth-child(1) > time:nth-child(2)",sep = "" )
             sub_block <- html_nodes(html_block, CSS_selector)
             date <- html_attr(x = sub_block,name = "datetime")
             date <- as.Date(date)   
             
-            # Extract pictures link
+            # image link and download
             CSS_selector <- paste("#",ID[i]," > a:nth-child(1) > img:nth-child(2)",sep = "" )
             sub_block <- html_nodes(html_block, CSS_selector)
             img_link <- html_attr(x = sub_block,name = "src")
             
-            #special parsing because for the end of the list the link is on an other attribut called "longdesc".
-            #also it is possible that no image was provided (img_link = NULL)
+            # special parsing because for the end of the list the link is on an other attribut called "longdesc".
+            # it is also possible that no image was provided (img_link = NULL)
             if(length(img_link) != 0){
                   if( img_link == "/img/transparent.gif"){
                         img_link <- html_attr(x = sub_block,name = "longdesc")
@@ -55,11 +56,12 @@ blocket_update <- function(blocket_url,bike_list){
       }
       bike_list
 }
+
 dba_update <- function(dba_url,bike_list){
       
       html_block <- read_html(dba_url)
       
-      #Extract ID (in dba the ID is found on child element, so the code structure change a little bit. 
+      # Extract ID (in dba the ID is found on child element, so the code structure change a little bit. 
       sub_block <- html_nodes(html_block, "tr.dbaListing > td:nth-child(2) > div:nth-child(3) > a:nth-child(1)")
       ID_ <- html_attr(x = sub_block,name = "href")
       ID_ <- substr(ID_,nchar(ID_)-13,nchar(ID_)-1)
@@ -68,10 +70,6 @@ dba_update <- function(dba_url,bike_list){
       j <- 1
       
       while (!(ID_[j] %in% bike_list$id) & !(j > length(ID_))){
-            print(j)
-            print(ID_[j])
-            print(!(ID_[j] %in% bike_list$id))
-            
             
             CSS_selector <- paste("tr.dbaListing:nth-child(",i,") > td:nth-child(2) > div:nth-child(3) > a:nth-child(1)",sep = "" )
             sub_block <- html_nodes(html_block, CSS_selector)
@@ -79,15 +77,17 @@ dba_update <- function(dba_url,bike_list){
             # In dba website, We cannot call the block directly by the ID, we need to iterate one by one on i, and check if the block is not empty.
             if(length(sub_block) != 0){
                   
-                  #title & link 
-                  
+                  # link 
                   link <- html_attr(x = sub_block,name = "href") 
+                  
+                  # title
                   splited_string <- strsplit(link, "/")
                   title <- splited_string[[1]][4]
+                  
+                  # ID
                   ID <- splited_string[[1]][5]
                   
-                  
-                  # Extract date
+                  # date
                   CSS_selector <- paste("tr.dbaListing:nth-child(",i,") > td[title=Dato]",sep = "" )  #CSS still works without quotes around "Dato"..
                   sub_block <- html_nodes(html_block, CSS_selector)
                   
@@ -106,8 +106,7 @@ dba_update <- function(dba_url,bike_list){
                         date <- as.Date(date)
                   }
                   
-                  
-                  # Extract pictures link
+                  # image link and download
                   CSS_selector <- paste("tr.dbaListing:nth-child(",i,") > td:nth-child(1) > div:nth-child(1) > a:nth-child(1) > div:nth-child(1)",sep = "" )  #CSS still works without quotes around "Dato"..
                   sub_block <- html_nodes(html_block, CSS_selector)
                   img_link <- html_attr(x = sub_block,name = "data-original")
@@ -115,7 +114,7 @@ dba_update <- function(dba_url,bike_list){
                                 destfile = paste("./pictures/",date, "_dba_", ID, ".jpg",sep = ""), 
                                 mode="wb")
                   
-                  #add row
+                  # Add row to bike_list
                   row_to_add <- data.frame( site = c("dba"),
                                             id = ID,
                                             title = title,
@@ -127,6 +126,63 @@ dba_update <- function(dba_url,bike_list){
                   j <- j + 1
             }
             i <- i + 1
+      }
+      bike_list
+}
+
+secondhandbikes_update <- function(secondhandbikes_url,bike_list){
+      
+      html_block <- read_html(secondhandbikes_url)
+      
+      #Extract ID table 
+      sub_block <- html_nodes(html_block, "div.serp-bike > a.bike-link")
+      ID <- html_attr(x = sub_block,name = "href")
+      ID <- substr(ID,nchar(ID)-3,nchar(ID))
+      
+      i <- 1
+      
+      while (!(ID[i] %in% bike_list$id) & !(i > length(ID))){
+            
+                  # link 
+                  CSS_selector <- paste("div.serp-bike:nth-child(",i,") > a:nth-child(1)",sep = "" )
+                  sub_block <- html_nodes(html_block, CSS_selector)
+                  link <- html_attr(x = sub_block,name = "href") 
+                  link <- paste("https://secondhandbikes.dk", link,sep = "")
+                  
+                  # title
+                  CSS_selector <- paste("div.serp-bike:nth-child(",i,") > a:nth-child(1) > div:nth-child(2) > span:nth-child(1)",sep = "" )
+                  sub_block <- html_nodes(html_block, CSS_selector)
+                  title <- html_text(sub_block) 
+                  title <- gsub(pattern = "^\\s+|\\s+$",replacement =  "", x = title)
+                  
+                  # image link
+                  CSS_selector <- paste("div.serp-bike:nth-child(",i,") > a:nth-child(1) > div:nth-child(1) > img:nth-child(1)",sep = "" )
+                  sub_block <- html_nodes(html_block, CSS_selector)
+                  img_link <- html_attr(x = sub_block,name = "src")
+                  img_link <- paste("https://secondhandbikes.dk", img_link,sep = "")
+                  
+                  # date
+                  date <- strsplit(x = img_link, split = "Thumbs/")[[1]][2]
+                  date <- substr(x = date,start = 1,stop = 8)
+                  date <- strptime(date, "%y-%m-%d")
+                  date <- as.Date(date)
+                  
+                  #download image
+                  download.file(url = img_link, 
+                                destfile = paste("./pictures/",date, "_2HB_", ID[i], ".jpg",sep = ""), 
+                                mode="wb")
+                  
+                  # Add row to bike_list
+                  row_to_add <- data.frame( site = c("secondhandbikes"),
+                                            id = ID[i],
+                                            title = title,
+                                            link = link,
+                                            img_link = img_link,
+                                            date = date)
+                  
+                  bike_list <- rbind(bike_list,row_to_add)
+            
+                  i <- i + 1
       }
       bike_list
 }
@@ -143,10 +199,13 @@ bike_list$date <- as.Date(bike_list$date)
 bike_list$id <- gsub(pattern = "^\\s+|\\s+$",replacement =  "", x = bike_list$id)  #just in case
 
 # blocket: filter -> All sweden + cyclar + (XT or Btwin) 
-bike_list <-blocket_update("https://www.blocket.se/hela_sverige?q=Btwin+OR+XT&cg=6060&w=3&st=s&c=&f=p&ca=23&is=1&l=0&md=th",bike_list)
+bike_list <- blocket_update("https://www.blocket.se/hela_sverige?q=Btwin+OR+XT&cg=6060&w=3&st=s&c=&f=p&ca=23&is=1&l=0&md=th",bike_list)
 
 # dba: filter -> All ?Denmark + cykler + XT
-bike_list <-dba_update("http://www.dba.dk/cykler/?soeg=xt&fra=privat&sort=listingdate-desc",bike_list)
+bike_list <- dba_update("http://www.dba.dk/cykler/?soeg=xt&fra=privat&sort=listingdate-desc",bike_list)
+
+# secondHandBikes: filter: Men's bike
+bike_list <- secondhandbikes_update("https://secondhandbikes.dk/Search/Bike?type=1&pricemin=0&pricemax=99999&city=-1&page=1",bike_list)
 
 # save result of the updated bike_list in a file
 write.csv(x = bike_list,file = "bike_list.csv",row.names = FALSE)
